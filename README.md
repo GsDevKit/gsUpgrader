d94e552ecc30800a39e5911a5753ad2944f17f2d
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2014-09-10 14:12:57 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+gsUpgrader
+=========
+
+Utility class to perform upgrades for GLASS, GLASS1 and Metacello
---
+++ tests/testGsUpgrader.sh
@@ -0,0 +1,109 @@
+#!/bin/bash -x
+
+set -e # exit on error
+
+startStone travis
+
+cd ${GS_HOME}/gemstone/stones/travis
+
+. defStone.env
+
+echo "=================================="
+echo "TESTING: upgradeGLASS, upgradeMetacello, upgradeGLASS1"
+echo "=================================="
+
+topaz -l -q -T50000 <<EOF
+iferr 1 stk
+iferr 2 stack
+iferr 3 exit 1
+login
+run
+Metacello new
+  baseline: 'GsUpgrader';
+  repository: 'filetree://${GS_HOME}/repository';
+  load.
+(Smalltalk at: #GsUpgrader) upgradeGLASS.
+(Smalltalk at: #GsUpgrader) upgradeMetacello.
+(Smalltalk at: #GsUpgrader) upgradeGLASS1.
+%
+
+exit 
+EOF
+
+stopStone travis
+stoneExtent travis
+startStone travis
+
+echo "=================================="
+echo "TESTING: upgradeGLASS, upgradeMetacello, upgradeGLASS1, upgradeGLASS, upgradeMetacello, upgradeGLASS1"
+echo "=================================="
+
+topaz -l -q -T50000 <<EOF
+iferr 1 stk
+iferr 2 stack
+iferr 3 exit 1
+login
+run
+Metacello new
+  baseline: 'GsUpgrader';
+  repository: 'filetree://${GS_HOME}/repository';
+load.
+(Smalltalk at: #GsUpgrader) upgradeGLASS.
+(Smalltalk at: #GsUpgrader) upgradeMetacello.
+(Smalltalk at: #GsUpgrader) upgradeGLASS1.
+(Smalltalk at: #GsUpgrader) upgradeGLASS.
+(Smalltalk at: #GsUpgrader) upgradeMetacello.
+(Smalltalk at: #GsUpgrader) upgradeGLASS1.
+%
+
+exit 
+EOF
+
+stopStone travis
+stoneExtent travis
+startStone travis
+
+echo "=================================="
+echo "TESTING: upgradeMetacello"
+echo "=================================="
+
+topaz -l -q -T50000 <<EOF
+iferr 1 stk
+iferr 2 stack
+iferr 3 exit 1
+login
+run
+Metacello new
+  baseline: 'GsUpgrader';
+    repository: 'filetree://${GS_HOME}/repository';
+      load.
+(Smalltalk at: #GsUpgrader) upgradeMetacello.
+%
+
+exit
+EOF
+
+stopStone travis
+stoneExtent travis
+startStone travis
+
+echo "=================================="
+echo "TESTING: upgradeGLASS1"
+echo "=================================="
+
+topaz -l -q -T50000 <<EOF
+iferr 1 stk
+iferr 2 stack
+iferr 3 exit 1
+login
+run
+Metacello new
+  baseline: 'GsUpgrader';
+      repository: 'filetree://${GS_HOME}/repository';
+            load.
+(Smalltalk at: #GsUpgrader) upgradeGLASS1.
+%
+
+exit 
+EOF
+
